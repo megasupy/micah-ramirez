5c971a57cf9efd45256d0ca1bd01fc573f4bb85c
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-02 21:15:31 -0400

    attempt to fix phone version

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://megasupy.github.io//"
+  base: "https://megasupy.github.io/"
 })
