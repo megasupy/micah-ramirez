e8061f3ba76fd052ca006fbafbdd8e476891c34c
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-04 00:25:26 -0400

    changed domain name

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,8 +7,9 @@ import "./index.css"
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/micah-ramirez" element={<Home />} /> {/* Similar to the stack navigator! the "/" path is just the root, your default page. */}
+      <Route path="/" element={<Home />} /> {/* Similar to the stack navigator! the "/" path is just the root, your default page. */}
       <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
 );
+9
