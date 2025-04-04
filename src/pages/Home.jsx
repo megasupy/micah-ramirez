86b61d1f1152beab5db4174e8e2ad164173c6a32
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-04 02:24:15 -0400

    added things to projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, }from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header.jsx";
 import "../index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,39 +7,48 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   useEffect(() => {
-    document.querySelector("#themeColor").setAttribute("content", "#E5E7EB")
+    document.querySelector("#themeColor").setAttribute("content", "#E5E7EB");
   });
+
+  const Background = ({ children }) => (
+    <div className="default-background-size default-background-color">
+      {children}
+    </div>
+  );
+  const IndentContainer = ({ children, className }) => (
+    <div className={className}>{children}</div>
+  );
   return (
-    <div class="default-background-size default-background-color">
+    <Background>
       <Header />
-      <div class="default-content-container top-nav-buffer">
-        <div class="indent-paragraph-container">
-          <div class="default-paragraph-div py-10">
-            <p class="default-text-color default-text-big font-bold">
+      <div className="default-content-container top-nav-buffer">
+        <IndentContainer className="indent-paragraph-container">
+          <div className="default-paragraph-div py-10">
+            <p className="default-text-color default-text-big font-bold">
               Hello! My name is{" "}
-              <span class="default-text-emphasis default-text-big font-bold">
+              <span className="default-text-emphasis default-text-big font-bold">
                 Micah Ramirez
               </span>
             </p>
           </div>
-          <div class="default-paragraph-div pt-8">
-            <p class="default-text-color font-bold pb-4">About Me</p>
-            <p class="default-text-color">
+          <div className="default-paragraph-div pt-8">
+            <p className="default-text-color font-bold pb-4">About Me</p>
+            <p className="default-text-color">
               I am a junior computer science student with a 3.9 GPA and a
               passion for developing efficient software. My coursework at UCF
               has given me a strong understanding of data structures and
               algorithms, and from my side projects I have acquired a solid
               foundation in full-stack development with React, Node.js, React
               Native, and Supabase. My ultimate goal is to improve lives with
-              software, so I am eager to apply my skills to make a
-              real impact.
+              software, so I am eager to apply my skills to make a real impact.
             </p>
           </div>
-          <div class="flex flex-row justify-center gap-16 pt-8 pb-8">
+          <div className="flex flex-row justify-center gap-16 pt-8 pb-8">
             <a
               href="https://www.linkedin.com/in/micah-ramirez-588174310/"
               target="_blank"
             >
+              {/**class instead of className because className doesn't work with the sizing for some reason */}
               <FontAwesomeIcon
                 icon={faLinkedin}
                 class="size-16 default-icons-color"
@@ -52,8 +61,20 @@ export default function Home() {
               />
             </a>
           </div>
-        </div>
+        </IndentContainer>
+        <IndentContainer className="indent-paragraph-container">
+          <div className="default-paragraph-div pt-8">
+            <p className="default-text-color font-bold pb-4">My Projects</p>
+            <p className="default-text-color">
+              1. Pantry Management App
+              <br />
+              2. Scalebot's Ascent
+              <br />
+              3. Something else.
+            </p>
+          </div>
+        </IndentContainer>
       </div>
-    </div>
-  );
+    </Background>
+  );9
 }
