8c6c4000134167dbff20557adf1dd0caacdec9b0
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-04 00:25:03 -0400

    changed domain name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://megasupy.github.io/micah-ramirez/",
+  base: "http://micahramirez.tech/",
 })
