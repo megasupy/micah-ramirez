ad2a23fe95760097339228fd8cb05405882031ff
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-13 23:38:00 -0400

    Display contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import Header from "../components/Header.jsx";
 import "../index.css";
 import AboutMe from "../components/AboutMe.jsx";
-import Projects from "../components/Projects.jsx";
 import Contact from "../components/Contact.jsx";
 
 export default function Home() {
@@ -10,32 +9,31 @@ export default function Home() {
     document.querySelector("#themeColor").setAttribute("content", "#E5E7EB");
   });
 
-  const Background = ({ children }) => (
+  const Background = ({children}) => (
     <div className="default-background-color default-background-size">{children}</div>
   );
-  const IndentContainer = ({ children, className }) => (
+  const IndentContainer = ({children, className}) => (
     <div className={className}>{children}</div>
   );
 
   const Copyright = () => (
     <p className="default-text-small text-center pb-4">
-      <br />© 2025 Micah Ramirez. All rights reserved.
+      <br/>© 2025 Micah Ramirez. All rights reserved.
     </p>
   );
 
   return (
     <Background>
-      <Header />
+      <Header/>
       <div className="default-content-container top-nav-buffer">
         <IndentContainer className="indent-paragraph-container">
-          <AboutMe />
-          <hr className="default-line-break" />
-          <Contact />
+          <AboutMe/>
+          <hr className="default-line-break"/>
+          <Contact/>
           {/*<Projects/>*/}
-          </IndentContainer>
-        <Copyright />
+        </IndentContainer>
+        <Copyright/>
       </div>
     </Background>
   );
-  9;
 }
