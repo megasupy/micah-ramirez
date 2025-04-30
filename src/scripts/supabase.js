dcdec47a3cac1f0f00997e72910edb329c3ad34a
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-04-30 13:23:17 -0400

    changed database, now uses my localhostdb. still testing, not ready for prod!

--- src/scripts/supabase.js
+++
@@ -1,6 +0,0 @@
-import {createClient} from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://uysujvmrkmyrvjbnpobh.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5c3Vqdm1ya215cnZqYm5wb2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMjk0MjcsImV4cCI6MjA1NzkwNTQyN30.r85IL-i6vL21n1-P0H5ZkHYwp05oInPBBe_5EzyDrN4"
-const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase;
