98f104767ab5fcd6ca07a6eaed13a5b8e910540a
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-05-20 15:47:11 -0400

    made small changes to text in about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,26 +9,27 @@ export default function AboutMe() {
       <div className="default-paragraph-div py-10">
         <p className="default-text-color default-text-big font-bold">
           Hello! My name is
-          <br/>
+          <br />
           <span className="default-text-emphasis default-text-big font-bold">
             Micah Ramirez
           </span>
         </p>
       </div>
-      <hr className="default-line-break"/>
+      <hr className="default-line-break" />
       <div className="default-paragraph-div">
         <p className="section-header-text">About Me</p>
         <p className="default-text-color">
-          I am a junior computer science student at University of Central Florida with a 3.9 GPA and a passion
-          for developing efficient software. My coursework at UCF has given me a
-          strong understanding of data structures and algorithms, and from my
-          side projects I have acquired a solid foundation in full-stack
-          development with React, Node.js, React Native, and Supabase. My
-          ultimate goal is to improve lives with software, so I am eager to
-          apply my skills to make a real impact.
+          I am a junior computer science student at University of Central
+          Florida with a 3.9 GPA and a passion for developing efficient
+          software. My coursework at UCF has given me a strong understanding of
+          data structures and algorithms, and from my side projects I have
+          acquired a solid foundation in full-stack development with React.js,
+          React Native, Node.js, PostgreSQL, and C#. My ultimate goal is to
+          improve lives with software, so I am eager to apply my skills to make
+          a real impact.
         </p>
       </div>
-      <Icons/>
+      <Icons />
     </>
   );
 }
